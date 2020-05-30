c3f7020fc6606951192d117163d9e1242b5b05a9
Peshotan Irani <peshotan_irani@sfu.ca>
2020-05-29 19:05:26 -0700

    generate pandas-profile reports

--- scripts/bc-covid-data.py
+++ scripts/bc-covid-data.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from pandas_profiling import ProfileReport
 
-PATH_TO_FILE = "PATH to your csv file (Preferably a URL from the internet)"
-FINAL_FILE_NAME = "Name of your dataset"
+PATH_TO_FILE = "http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv"
+FINAL_FILE_NAME = "BC-COVID19"
 FINAL_HTML_PATH = "../webpages/" + FINAL_FILE_NAME + ".html"
 
 df = pd.read_csv(PATH_TO_FILE, error_bad_lines=False)
--- scripts/oxford-dataset.py
+++ scripts/oxford-dataset.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from pandas_profiling import ProfileReport
 
-PATH_TO_FILE = "PATH to your csv file (Preferably a URL from the internet)"
-FINAL_FILE_NAME = "Name of your dataset"
+PATH_TO_FILE = "https://github.com/OxCGRT/covid-policy-tracker/raw/master/data/OxCGRT_latest.csv"
+FINAL_FILE_NAME = "oxfordDataset-COVID19-response"
 FINAL_HTML_PATH = "../webpages/" + FINAL_FILE_NAME + ".html"
 
 df = pd.read_csv(PATH_TO_FILE, error_bad_lines=False)
