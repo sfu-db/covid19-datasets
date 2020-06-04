aaf4cb4832025795960420edf15d127758321cc2
Peshotan Irani <59293179+peterirani@users.noreply.github.com>
2020-06-04 13:30:31 -0700

    update(pandas-profile-template.py) : add additional instructions

--- assets/pandas-profile-template.py
+++ assets/pandas-profile-template.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 from pandas_profiling import ProfileReport
 
+#fill only the following 2 variables (PATH_TO_FILE and FINAL_FILE_NAME)
 PATH_TO_FILE = "PATH to your csv file (Preferably a URL from the internet)"
-FINAL_FILE_NAME = "Name of your dataset"
+FINAL_FILE_NAME = "Name of your dataset (without .html extension)"
+
+# do not touch the rest of the code
 FINAL_HTML_PATH = "../webpages/" + FINAL_FILE_NAME + ".html"
 
 df = pd.read_csv(PATH_TO_FILE, error_bad_lines=False)
